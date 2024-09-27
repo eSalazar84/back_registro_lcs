e3ed2fa4d4fcd8c2b13a1ba61ab780b2086680ff
fabricio <fabricio.cbe@gmail.com>
2024-09-27 08:57:50 -0300

    se actualiza entidad casa con sus dto y agrega enums

--- src/casa/dto/update-casa.dto.ts
+++ src/casa/dto/update-casa.dto.ts
@@ -1,4 +1,46 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCasaDto } from './create-casa.dto';
+import { IsBoolean, IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Localidad } from '../enum/localidad.enum';
+import { Estado_vivienda } from '../enum/estado_vivienda.enum';
+import { Alquiler } from '../enum/alquiler.enum';
 
-export class UpdateCasaDto extends PartialType(CreateCasaDto) {}
+export class UpdateCasaDto extends PartialType(CreateCasaDto) {
+
+    idCasa: number
+
+    @IsString()
+    @IsNotEmpty()
+    direccion?: string
+
+    @IsNumber()
+    numero_direccion?: number
+
+    @IsBoolean()
+    departamento?: boolean
+
+    @IsString()
+    @IsNotEmpty()
+    numero_departamento: string
+
+    @IsBoolean()
+    alquiler: boolean
+
+    @IsNumber()
+    valor_alquiler: number
+
+    @IsEnum(Localidad)
+    @IsNotEmpty()
+    Localidad: Localidad
+    
+    @IsNumber()
+    cantidad_dormitorios: number
+
+    @IsEnum(Estado_vivienda)
+    estado_vivienda: Estado_vivienda
+
+    @IsEnum(Alquiler)
+    tipo_alquiler: Alquiler
+
+
+}
---
+++ src/casa/enum/estado_vivienda.enum.ts
@@ -0,0 +1,7 @@
+export enum Estado_vivienda{
+    Muy_bueno = "Muy bueno",
+    Bueno = "Bueno",
+    Regular = "Regular",
+    Malo = "Malo",
+    Muy_malo = "Muy malo"
+}
