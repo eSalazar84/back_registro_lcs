5a59cafb4afac50203f2fa0e9c269865c54955a2
fabricio <fabricio.cbe@gmail.com>
2024-09-30 11:34:03 -0300

    se cambia nombre de la entidad casa a vivienda, e crea y modifica una vivienda

--- src/ingreso/ingreso.module.ts
+++ src/ingreso/ingreso.module.ts
@@ -3,12 +3,12 @@ import { IngresoService } from './ingreso.service';
 import { IngresoController } from './ingreso.controller';
 import { Ingreso } from './entities/ingreso.entity';
 import { Persona } from 'src/persona/entities/persona.entity';
-import { Casa } from 'src/casa/entities/casa.entity';
+import { Vivienda } from 'src/casa/entities/vivienda.entity';
 import { Lote } from 'src/lote/entities/lote.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Lote,Casa,Persona,Ingreso])],
+  imports:[TypeOrmModule.forFeature([Lote,Vivienda,Persona,Ingreso])],
   controllers: [IngresoController],
   providers: [IngresoService],
 })
