438dc6c7903b650c7cb0abc5d9360b98ccd18ee0
fabricio <fabricio.cbe@gmail.com>
2024-10-18 11:22:25 -0300

    cre crea persona sin repetir viviendas

--- src/registro/registro.service.ts
+++ src/registro/registro.service.ts
@@ -1,4 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Connection } from 'typeorm';
 import { Persona } from 'src/persona/entities/persona.entity';
 import { CreatePersonaDto } from 'src/persona/dto/create-persona.dto';
 import { CreateViviendaDto } from 'src/vivienda/dto/create-vivienda.dto';
@@ -8,93 +10,154 @@ import { PersonaService } from 'src/persona/persona.service';
 import { ViviendaService } from 'src/vivienda/vivienda.service';
 import { LoteService } from 'src/lote/lote.service';
 import { IngresoService } from 'src/ingreso/ingreso.service';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class RegistroService {
     constructor(
         @InjectRepository(Persona)
-        private readonly personaRepository: Repository<Persona>,
-        private personaService: PersonaService,
-        private viviendaService: ViviendaService,
-        private loteService: LoteService,
-        private ingresoService: IngresoService
-    ) { }
+        private readonly personaService: PersonaService,
+        private readonly viviendaService: ViviendaService,
+        private readonly loteService: LoteService,
+        private readonly ingresoService: IngresoService,       
+    ) {}
 
-    async createAll(
-        createPersonaDto: CreatePersonaDto,
-        viviendaDto: CreateViviendaDto,
-        ingresoDtos: CreateIngresoDto[],
-        loteDto: CreateLoteDto,
-    ): Promise<Persona> {
-        try {
-            console.log(
-                'Inicio de createAll:',
-                'Persona', createPersonaDto,
-                'Vivienda', viviendaDto,
-                'Ingresos', ingresoDtos,
-                'Lote', loteDto
-            );
-
-            // Verificar si ya existe una persona con el mismo DNI
-            console.log("Buscando persona con DNI:", createPersonaDto.dni);
-            const personaFound = await this.personaService.findOneByDni(createPersonaDto.dni);
-            console.log("Resultado de persona encontrada por DNI:", personaFound);
-
-            // Si la persona es encontrada, lanzar excepción. Si no, continuar.
-            if (personaFound) {
-                throw new HttpException({
-                    status: HttpStatus.BAD_REQUEST,
-                    error: `La persona con DNI ${createPersonaDto.dni} ya está registrada.`,
-                }, HttpStatus.BAD_REQUEST);
-            }
-
-            // Continuar con la creación de la vivienda y la persona
-            console.log('No se encontró persona con el DNI, se procede con la creación.');
-
-            // Crear la vivienda primero
-            const vivienda = await this.viviendaService.createVivienda(viviendaDto);
-            console.log('Vivienda creada:', vivienda);
-
-            // Crear el lote solo si la persona es titular
-            let idLote: number | null = null;
-            if (createPersonaDto.titular_cotitular === 'Titular') {
-                const lote = await this.loteService.createLote(loteDto);
-               
-                console.log('Lote creado:', lote);
-                idLote = lote.idLote; // Asignar el ID del lote solo si es titular
-                console.log("id lote", idLote);
-                
-            } else {
-                console.log('Persona es cotitular, no se crea lote.');
-            }
-
-            // Crear la persona pasando el ID de lote (o null si es cotitular)
-            const persona = await this.personaService.createPersona(createPersonaDto, vivienda.idVivienda, idLote);
-            console.log("id lote en persona", idLote);
-            
-            console.log('Persona creada:', persona);
-
-            // Crear y guardar los ingresos asociados a la persona
-            const ingresos = await this.ingresoService.createIngreso(ingresoDtos, persona.idPersona);
-            console.log('Ingresos creados.', ingresos);
-
-            console.log('Fin de createAll. Persona retornada:', persona);
-            return persona;
-
-        } catch (error) {
-            console.error("Error detectado en el flujo de createAll:", error); // Log extendido de error
-
-            if (error instanceof HttpException) {
-                throw error;  // Lanza el error tal cual si ya es una HttpException
-            }
-
-            throw new HttpException({
-                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                error: `Ocurrió un error al crear la nueva dependencia`,
-            }, HttpStatus.INTERNAL_SERVER_ERROR);
+    // Función para calcular la edad a partir de la fecha de nacimiento
+    private calcularEdad(fechaNacimiento: Date): number {
+        const hoy = new Date();
+        const fechaNac = new Date(fechaNacimiento);
+        let edad = hoy.getFullYear() - fechaNac.getFullYear();
+        const mes = hoy.getMonth() - fechaNac.getMonth();
+        if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+            edad--; // Ajustar si no ha pasado el cumpleaños este año
         }
+        return edad;
     }
 
+    async createAll(
+        personas: {
+            persona: CreatePersonaDto,
+            vivienda: CreateViviendaDto,
+            ingresos?: CreateIngresoDto[],
+            lote?: CreateLoteDto
+        }[]
+    ): Promise<Persona[]> {
+        const createdPersonas: Persona[] = [];
+        const viviendasCreadas: { [key: string]: any } = {}; // Objeto para almacenar viviendas por clave
+        const viviendasVerificadas: { [key: string]: boolean } = {}; // Objeto para almacenar viviendas verificadas
+
+        // Verificación de si la vivienda ya existe, antes del try
+        if (personas.length > 0) {
+            for (const personaData of personas) {
+                const { vivienda } = personaData;
+
+                // Crear una clave única para identificar la vivienda
+                const viviendaKey = `${vivienda.direccion}-${vivienda.numero_direccion}-${vivienda.localidad}`;
+
+                // Si ya se verificó esta vivienda en el proceso, continuar con la siguiente
+                if (viviendasVerificadas[viviendaKey]) {
+                    continue;
+                }
+
+                // Buscar la vivienda en la base de datos
+                const viviendaFound = await this.viviendaService.findByAddress(
+                    vivienda.direccion,
+                    vivienda.numero_direccion,
+                    vivienda.localidad
+                );
+
+                if (viviendaFound) {
+                    // Si se encuentra una vivienda registrada, lanzamos una excepción
+                    console.error(`La vivienda en ${vivienda.direccion}, ${vivienda.numero_direccion}, ${vivienda.localidad} ya está registrada.`);
+                    throw new HttpException({
+                        status: HttpStatus.BAD_REQUEST,
+                        error: `La vivienda en ${vivienda.direccion}, ${vivienda.numero_direccion}, ${vivienda.localidad} ya está registrada.`,
+                    }, HttpStatus.BAD_REQUEST);
+                }
+
+                // Marcar esta vivienda como verificada (no está registrada en la base de datos)
+                viviendasVerificadas[viviendaKey] = true;
+            }
+
+            // Verificar si alguna de las personas ya está registrada
+            for (const personaData of personas) {
+                const { persona } = personaData;
+
+                const personaFound = await this.personaService.findOneByDni(persona.dni);
+                if (personaFound) {
+                    console.log(`La persona con DNI ${persona.dni} ya está registrada.`);
+                    throw new HttpException({
+                        status: HttpStatus.BAD_REQUEST,
+                        error: `La persona con DNI ${persona.dni} ya está registrada.`,
+                    }, HttpStatus.BAD_REQUEST);
+                }
+            }
+
+            // Si no se encuentra la vivienda y ninguna persona está registrada, continuar con la creación
+            try {
+                for (const personaData of personas) {
+                    const { persona, vivienda, ingresos, lote } = personaData;
+
+                    console.log(
+                        'Inicio de createAll:',
+                        'Persona', persona,
+                        'Vivienda', vivienda,
+                        'Ingresos', ingresos,
+                        'Lote', lote
+                    );
+
+                    // Crear una clave única para la vivienda
+                    const viviendaKey = `${vivienda.direccion}-${vivienda.numero_direccion}-${vivienda.localidad}`;
+
+                    // Verificar si la vivienda ya fue creada previamente en este proceso
+                    if (!viviendasCreadas[viviendaKey]) {
+                        // Si la vivienda no fue encontrada previamente, crearla
+                        const viviendaCreada = await this.viviendaService.createVivienda(vivienda);
+                        console.log('Vivienda creada:', viviendaCreada);
+                        viviendasCreadas[viviendaKey] = viviendaCreada; // Almacenar la vivienda creada
+                    } else {
+                        console.log('Vivienda ya existe en la base de datos o fue creada previamente.');
+                    }
+
+                    const viviendaReutilizada = viviendasCreadas[viviendaKey]; // Obtener la vivienda reutilizada
+
+                    // Crear el lote solo si la persona es titular
+                    let idLote: number | null = null;
+                    if (persona.titular_cotitular === 'Titular') {
+                        const loteFound = await this.loteService.createLote(lote);
+                        console.log('Lote creado:', lote);
+                        idLote = loteFound.idLote; // Asignar el ID del lote solo si es titular
+                        console.log("id lote", idLote);
+                    } else {
+                        console.log('Persona es cotitular, no se crea lote.');
+                    }
+
+                    // Crear la persona y asignar la vivienda existente
+                    const personaCreada = await this.personaService.createPersona(persona, viviendaReutilizada.idVivienda, idLote);
+                    console.log('Persona creada:', personaCreada);
+
+                    // Crear los ingresos solo si la persona es mayor de edad (18 años o más) y se han proporcionado ingresos
+                    const edad = this.calcularEdad(persona.fecha_nacimiento);
+                    if (edad >= 18 && ingresos && ingresos.length > 0) {
+                        const ingresosCreados = await this.ingresoService.createIngreso(ingresos, personaCreada.idPersona);
+                        console.log('Ingresos creados.', ingresosCreados);
+                    } else {
+                        console.log('Persona es menor de edad o no tiene ingresos, no se crean ingresos.');
+                    }
+
+                    // Añadir la persona creada al array de resultados
+                    createdPersonas.push(personaCreada);
+                }
+
+                console.log('Fin de createAll. Personas retornadas:', createdPersonas);
+                return createdPersonas;
+
+            } catch (error) {
+                console.error("Error detectado en el flujo de createAll:", error);
+                throw new HttpException({
+                    status: HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: `Ocurrió un error al crear las nuevas dependencias`,
+                }, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
 }
