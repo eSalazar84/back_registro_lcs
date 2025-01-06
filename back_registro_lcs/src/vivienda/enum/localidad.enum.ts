cd86e8e21d1a0a4231ba09d3f6b36998c09bbac9
Anabel <assannanabel@gmail.com>
2025-01-06 14:25:23 -0300

    cambios

--- back_registro_lcs/src/vivienda/enum/localidad.enum.ts
+++ back_registro_lcs/src/vivienda/enum/localidad.enum.ts
@@ -1,9 +1,8 @@
-export enum Localidad {
+export enum Localidad { //partido de Benito Juárez
     Barker = 'Barker',
     Villa_Cacique = 'Villa Cacique',
     Benito_Juarez = 'Benito Juarez',
     Estacion_Lopez = 'Estacion Lopez',
-    Bunge = 'Coronel Rodolfo Bunge',
     El_Luchador = 'El Luchador',
     Tedin_Uriburu = 'Tedin Uriburu'
 }
--- src/vivienda/enum/localidad.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum Localidad { //partido de Benito Juárez
-    Barker = 'Barker',
-    Villa_Cacique = 'Villa Cacique',
-    Benito_Juarez = 'Benito Juarez',
-    Estacion_Lopez = 'Estacion Lopez',
-    El_Luchador = 'El Luchador',
-    Tedin_Uriburu = 'Tedin Uriburu'
-}
