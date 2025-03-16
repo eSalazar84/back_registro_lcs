1fbadc6e60a8dfc28a4fdf5bd6e81ae351f8811e
Emiliano Salazar <salazaremiliano84@gmail.com>
2025-03-16 19:41:19 -0300

    se hacen ultimas modificaciones de estilado segun pedido cliente

--- frontend/src/componentes/nav/Nav.jsx
+++ frontend/src/componentes/nav/Nav.jsx
@@ -3,6 +3,7 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { FaPhoneAlt } from "react-icons/fa";
 
 
 
@@ -18,7 +19,9 @@ function Nav() {
         <>
             <div className={styles.container_general_nav}>
                 <div className={styles.logo}>
-                    <img src="/Logo_Muni2024.png" alt="logo" />
+                    <Link to={"/"} >
+                        <img src="/Logo_Muni2024.png" alt="logo" />
+                    </Link>
                 </div>
 
                 <div className={styles.container_h2_nav}>
@@ -43,6 +46,9 @@ function Nav() {
                     </a>
                 </div>
 
+                <a href="https://benitojuarez.gov.ar/telefonos-utiles/" className={styles.numero_texto}>
+                    <FaPhoneAlt className={styles.icon_emergencias} />
+                </a>
                 <div className={styles.container_fecha}>
                     <p>{fechaActual}</p>
                 </div>
